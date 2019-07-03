a7e589b0d12caa18820bbe3ffcc6e759a152bf01
adis7102 <adis7102@gmail.com>
2019-07-03 15:15:37 +0700

    cruding monster table

--- controllers/controller-monster.js
+++ controllers/controller-monster.js
@@ -1,5 +1,35 @@
+const Model = require('../models/index')
+const Monster = Model.Monster
 class ControllerMonster {
+    static getAll(req,res){
+        Monster.findAll()
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err =>{
+            res.send(err.message)
+        })
+    }
 
+    static findMonster(req,res){
+        Monster.findByPk(req.parms.id)
+        .then(dataMonster => {
+            res.render('testView.ejs',{dataMonster})
+        })
+        .catch(err =>{
+            res.send(err.message)
+        })
+    }
+
+    static updateMonster(req,res){
+        Monster.update(req.body,{where : {'id' : req.body.id}})
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err =>{
+            res.send(err.message)
+        })
+    }
 }
-
+ControllerMonster 
 module.exports = ControllerMonster
